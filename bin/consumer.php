ad0525311f6d8d0f523bde612a99ad32a17e8bb0
Dominik Chrastecky <dominik@chrastecky.cz>
2023-08-10 01:35:34 +0200

    Switch to event bridge

--- bin/consumer.php
+++ bin/consumer.php
@@ -1,14 +1,15 @@
 <?php declare(strict_types=1);
 
-use Bref\Symfony\Messenger\Service\Sqs\SqsConsumer;
+use App\Kernel;
+use Bref\Symfony\Messenger\Service\EventBridge\EventBridgeConsumer;
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
-(new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+(new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 
-$kernel = new \App\Kernel($_SERVER['APP_ENV'], (bool)$_SERVER['APP_DEBUG']);
+$kernel = new Kernel($_SERVER['APP_ENV'], (bool)$_SERVER['APP_DEBUG']);
 $kernel->boot();
 
 // Return the Bref consumer service
-return $kernel->getContainer()->get(SqsConsumer::class);
+return $kernel->getContainer()->get(EventBridgeConsumer::class);
