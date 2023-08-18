41326cd2fbb42b9573e824812e983b0e22fff860
Dominik Chrastecky <dominik@chrastecky.cz>
2023-08-18 14:20:18 +0200

    Fix loading user from cookie

---
+++ src/Authentication/CookieAuthenticator.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Authentication;
+
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
+use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
+use Symfony\Component\Security\Http\SecurityRequestAttributes;
+
+final class CookieAuthenticator extends AbstractAuthenticator
+{
+    public function __construct(
+        private readonly UrlGeneratorInterface $urlGenerator,
+    ) {
+    }
+
+    public function supports(Request $request): ?bool
+    {
+        return $request->cookies->has(UserProvider::COOKIE_NAME);
+    }
+
+    public function authenticate(Request $request): Passport
+    {
+        $cookie = $request->cookies->get(UserProvider::COOKIE_NAME);
+        assert(is_string($cookie));
+        $value = json_decode($cookie, true, JSON_THROW_ON_ERROR);
+
+        return new SelfValidatingPassport(
+            new UserBadge(
+                "{$value['username']}@{$value['instance']}",
+                static fn () => new User($value['username'], $value['instance'], $value['jwt']),
+            )
+        );
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
+    {
+        return null;
+    }
+
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
+    {
+        if ($request->hasSession()) {
+            $request->getSession()->set(SecurityRequestAttributes::AUTHENTICATION_ERROR, $exception);
+        }
+
+        return new RedirectResponse($this->urlGenerator->generate('auth.login'));
+    }
+}
