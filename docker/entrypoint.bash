8bd6b5fa2d7e2f90c82875d1364e266072371c46
Dominik Chrastecky <dominik@chrastecky.cz>
2023-08-30 18:46:06 +0200

    Add messenger consumer

--- docker/entrypoint.bash
+++ docker/entrypoint.bash
@@ -5,4 +5,8 @@ cd /var/www/html || exit 1
 rm -rf var
 php bin/console cache:warmup
 
+supervisorctl reread
+supervisorctl update
+supervisorctl start messenger-consume:*
+
 exec apache2ctl -D FOREGROUND
