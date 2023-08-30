e0ec07610255a240ec80fff7863272e384e046dd
Dominik Chrastecky <dominik@chrastecky.cz>
2023-08-30 18:58:13 +0200

    Start supervisor

--- docker/entrypoint.bash
+++ docker/entrypoint.bash
@@ -5,6 +5,7 @@ cd /var/www/html || exit 1
 rm -rf var
 php bin/console cache:warmup
 
+/etc/init.d/supervisor start
 supervisorctl reread
 supervisorctl update
 supervisorctl start messenger-consume:*
