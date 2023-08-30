62239b07e34416b22ff80ab903d1166f05dd9faf
Dominik Chrastecky <dominik@chrastecky.cz>
2023-08-30 20:17:16 +0200

    Add messenger output, chown data

--- docker/entrypoint.bash
+++ docker/entrypoint.bash
@@ -10,4 +10,8 @@ supervisorctl reread
 supervisorctl update
 supervisorctl start messenger-consume:*
 
+[ -z ${APP_CACHE_DIR+x} ] || chown -R 33:33 "$APP_CACHE_DIR"
+[ -z ${APP_LOG_DIR+x} ] || chown -R 33:33 "$APP_LOG_DIR"
+[ -z ${LOCAL_FILE_UPLOADER_PATH+x} ] || chown -R 33:33 "$LOCAL_FILE_UPLOADER_PATH"
+
 exec apache2ctl -D FOREGROUND
