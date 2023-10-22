7f2ca71b60bd654c5b579e172a488f4fc06fa3de
Rikudou_Sage <dominik@chrastecky.cz>
2023-10-22 12:35:01 +0200

    Feat: Add reports for user posts (#19)

--- src/JobHandler/ReportUnreadPostsJobHandler.php
+++ src/JobHandler/ReportUnreadPostsJobHandler.php
@@ -36,8 +36,8 @@ final readonly class ReportUnreadPostsJobHandler
     {
         try {
             $api = $this->apiFactory->get(instance: $job->instance, jwt: $job->jwt);
-            $targetUser = $api->site()->getSite()->myUser?->localUserView?->person;
-            if ($targetUser === null) {
+            $recipient = $api->site()->getSite()->myUser?->localUserView?->person;
+            if ($recipient === null) {
                 throw new RuntimeException('The target user was not found.');
             }
 
@@ -46,21 +46,38 @@ final readonly class ReportUnreadPostsJobHandler
                 return;
             }
 
-            $communityInstance = parse_url($job->community->actorId, PHP_URL_HOST);
-            $communityUrl = "https://{$job->instance}/c/{$job->community->name}@{$communityInstance}";
-            $message = "Here is a list of unread posts from [!{$job->community->name}@{$communityInstance}]({$communityUrl}):\n";
+            if (!$job->person && !$job->community) {
+                return;
+            }
+
+            $message = 'Here is a list of unread posts from ';
+            if ($job->person) {
+                $personInstance = parse_url($job->person->actorId, PHP_URL_HOST);
+                $personUrl = "https://{$job->instance}/u/{$job->person->name}@{$personInstance}";
+
+                $message .= "[@{$job->person->name}@{$personInstance}]({$personUrl})";
+            }
+            if ($job->person && $job->community) {
+                $message .= ' in ';
+            }
+            if ($job->community) {
+                $communityInstance = parse_url($job->community->actorId, PHP_URL_HOST);
+                $communityUrl = "https://{$job->instance}/c/{$job->community->name}@{$communityInstance}";
+                $message .= "[!{$job->community->name}@{$communityInstance}]({$communityUrl})";
+            }
+            $message .= ":\n";
             foreach ($unread as $post) {
                 assert($post instanceof Post);
                 $message .= "- [{$post->name}](https://{$job->instance}/post/{$post->id})\n";
             }
 
-            $this->botApi->currentUser()->sendPrivateMessage(recipient: $targetUser, content: $message);
+            $this->botApi->currentUser()->sendPrivateMessage(recipient: $recipient, content: $message);
         } finally {
-            if (isset($targetUser) && ($expression = $job->scheduleExpression)) {
+            if (isset($recipient) && ($expression = $job->scheduleExpression)) {
                 sleep(1);
                 assert($job->scheduleTimezone !== null);
 
-                $this->currentUserService->setCurrentUser(new User($targetUser->name, $job->instance, $job->jwt));
+                $this->currentUserService->setCurrentUser(new User($recipient->name, $job->instance, $job->jwt));
 
                 $nextDate = $this->scheduleExpressionParser->getNextRunDate(
                     expression: $expression,
@@ -80,22 +97,42 @@ final readonly class ReportUnreadPostsJobHandler
 
         $maxPage = 10;
         $currentPage = 1;
-        while ($currentPage < $maxPage) {
-            $response = $api->post()->getPosts(
-                community: $job->community,
-                page: $currentPage,
-                sort: SortType::New,
-                listingType: ListingType::All,
-            );
-            foreach ($response as $post) {
-                if ($post->read) {
-                    continue;
+        if ($job->community && !$job->person) {
+            while ($currentPage < $maxPage) {
+                $response = $api->post()->getPosts(
+                    community: $job->community,
+                    page: $currentPage,
+                    sort: SortType::New,
+                    listingType: ListingType::All,
+                );
+                foreach ($response as $post) {
+                    if ($post->read) {
+                        continue;
+                    }
+
+                    yield $post->post;
                 }
 
-                yield $post->post;
+                ++$currentPage;
             }
+        } elseif ($job->person) {
+            while ($currentPage < $maxPage) {
+                $response = $api->user()->getPosts(
+                    user: $job->person,
+                    community: $job->community,
+                    page: $currentPage,
+                    sort: SortType::New,
+                );
+                foreach ($response as $post) {
+                    if ($post->read) {
+                        continue;
+                    }
 
-            ++$currentPage;
+                    yield $post->post;
+                }
+
+                ++$currentPage;
+            }
         }
     }
 }
