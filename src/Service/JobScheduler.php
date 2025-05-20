bb57e7ea4193e1e542ccd814ee39bc60c2476563
Dominik Chrastecky <dominik@chrastecky.cz>
2025-05-20 16:43:23 +0200

    Replace part

--- src/Service/JobScheduler.php
+++ src/Service/JobScheduler.php
@@ -5,6 +5,7 @@ namespace App\Service;
 use DateTimeInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Stamp\DelayStamp;
+use Symfony\Component\Messenger\Stamp\StampInterface;
 
 final readonly class JobScheduler
 {
@@ -15,7 +16,13 @@ final readonly class JobScheduler
 
     public function schedule(object $job, DateTimeInterface $dateTime): void
     {
+        /** @var array<StampInterface> $stamps */
+        $stamps = [];
         $delayStamp = DelayStamp::delayUntil($dateTime);
-        $this->messageBus->dispatch($job, [$delayStamp]);
+        if ($delayStamp->getDelay() > 0) {
+            $stamps[] = $delayStamp;
+        }
+
+        $this->messageBus->dispatch($job, $stamps);
     }
 }
